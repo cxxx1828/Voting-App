4bae7f95de18701d72c98678f1fd9eead41eb9d0
cxxx1828 <cxxx1828@gmail.com>
2025-09-21 11:15:18 +0200

    Dodati HttpHelper i network_security_config

---
+++ app/src/main/java/com/nina/dragicevic/HttpHelper.java
@@ -0,0 +1,200 @@
+package com.nina.dragicevic;
+
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpHelper {
+
+    private static final int SUCCESS = 200;
+    private static final String TAG = "HTTP_HELPER";
+    public static final String BASE_URL = "http://10.0.2.2:8080/api"; // 10.0.2.2 for emulator, localhost for device
+
+    /*HTTP get json Array*/
+    public JSONArray getJSONArrayFromURL(String urlString) throws IOException, JSONException {
+        HttpURLConnection urlConnection = null;
+        try {
+            Log.d(TAG, "GET Array request to: " + urlString);
+            java.net.URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            /*header fields*/
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("Accept", "application/json");
+            urlConnection.setReadTimeout(10000 /* milliseconds */ );
+            urlConnection.setConnectTimeout(15000 /* milliseconds */ );
+
+            urlConnection.connect();
+
+            int responseCode = urlConnection.getResponseCode();
+            Log.d(TAG, "Response code: " + responseCode);
+
+            if (responseCode != SUCCESS) {
+                Log.e(TAG, "HTTP Error: " + responseCode);
+                return null;
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            br.close();
+
+            String jsonString = sb.toString();
+            Log.d(TAG, "JSON Array response: " + jsonString);
+
+            return new JSONArray(jsonString);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Network error in getJSONArrayFromURL", e);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+    }
+
+    /*HTTP get json object*/
+    public JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException {
+        HttpURLConnection urlConnection = null;
+        try {
+            Log.d(TAG, "GET Object request to: " + urlString);
+            java.net.URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            /*header fields*/
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("Accept", "application/json");
+            urlConnection.setReadTimeout(10000 /* milliseconds */ );
+            urlConnection.setConnectTimeout(15000 /* milliseconds */ );
+
+            urlConnection.connect();
+
+            int responseCode = urlConnection.getResponseCode();
+            Log.d(TAG, "Response code: " + responseCode);
+
+            if (responseCode != SUCCESS) {
+                Log.e(TAG, "HTTP Error: " + responseCode);
+                return null;
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            br.close();
+
+            String jsonString = sb.toString();
+            Log.d(TAG, "JSON Object response: " + jsonString);
+
+            return new JSONObject(jsonString);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Network error in getJSONObjectFromURL", e);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+    }
+
+    /*HTTP post*/
+    public JSONObject postJSONObjectFromURL(String urlString, JSONObject jsonObject) throws IOException, JSONException {
+        HttpURLConnection urlConnection = null;
+        try {
+            Log.d(TAG, "POST request to: " + urlString);
+            Log.d(TAG, "POST body: " + jsonObject.toString());
+
+            java.net.URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+            urlConnection.setRequestProperty("Accept","application/json");
+
+            /*needed when used POST or PUT methods*/
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+
+            urlConnection.connect();
+
+            DataOutputStream os = new DataOutputStream(urlConnection.getOutputStream());
+            /*write json object*/
+            os.writeBytes(jsonObject.toString());
+            os.flush();
+            os.close();
+
+            int responseCode = urlConnection.getResponseCode();
+            Log.d(TAG, "POST Response code: " + responseCode);
+
+            if (responseCode != SUCCESS && responseCode != 201) {
+                Log.e(TAG, "HTTP POST Error: " + responseCode);
+                return null;
+            }
+
+            // Read response
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            br.close();
+
+            String jsonString = sb.toString();
+            Log.d(TAG, "POST Response: " + jsonString);
+
+            return new JSONObject(jsonString);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Network error in postJSONObjectFromURL", e);
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+    }
+
+    /*HTTP delete*/
+    public boolean httpDelete(String urlString) throws IOException, JSONException {
+        HttpURLConnection urlConnection = null;
+        try {
+            Log.d(TAG, "DELETE request to: " + urlString);
+            java.net.URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            urlConnection.setRequestProperty("Accept","application/json");
+
+            urlConnection.connect();
+
+            int responseCode = urlConnection.getResponseCode();
+            Log.d(TAG, "DELETE Response code: " + responseCode);
+
+            return (responseCode == SUCCESS);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Network error in httpDelete", e);
+            return false;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+    }
+}
