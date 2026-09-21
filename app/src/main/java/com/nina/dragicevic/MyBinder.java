b1a9657b5ed93e82de4d55dcd44cc34ba1fe642d
Nina Dragićević <cxxx1828@gmail.com>
2026-09-21 12:55:23 +0200

    Update MyBinder.java

--- app/src/main/java/com/nina/dragicevic/MyBinder.java
+++ app/src/main/java/com/nina/dragicevic/MyBinder.java
@@ -8,25 +8,21 @@ public class MyBinder extends Binder {
     private boolean serviceRunning = false;
     private int checkedSessionsCount = 0;
 
-    //Dobija trenutni status servisa
 
     public boolean isServiceRunning() {
         return serviceRunning;
     }
 
-    //Postavlja status servisa
 
     public void setServiceStatus(boolean isRunning) {
         this.serviceRunning = isRunning;
     }
 
-    //Dobija broj proverenih sesija
 
     public int getCheckedSessionsCount() {
         return checkedSessionsCount;
     }
 
-    //Postavlja broj proverenih sesija
     public void setCheckedSessionsCount(int count) {
         this.checkedSessionsCount = count;
     }
