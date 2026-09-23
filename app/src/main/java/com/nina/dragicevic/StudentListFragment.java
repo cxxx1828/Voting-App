24dbef377425cf39c35b9962d1057e6240e5a54d
Nina Dragićević <cxxx1828@gmail.com>
2026-09-23 22:05:08 +0200

    Update StudentListFragment.java

--- app/src/main/java/com/nina/dragicevic/StudentListFragment.java
+++ app/src/main/java/com/nina/dragicevic/StudentListFragment.java
@@ -14,11 +14,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StudentListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class StudentListFragment extends Fragment {
 
     ListView lista;
@@ -28,12 +24,9 @@ public class StudentListFragment extends Fragment {
     StudentAdapter adapter;
     DecideItDbHelper dbHelper;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -41,15 +34,6 @@ public class StudentListFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StudentListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static StudentListFragment newInstance(String param1, String param2) {
         StudentListFragment fragment = new StudentListFragment();
         Bundle args = new Bundle();
